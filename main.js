ced655e13193bb5f54b406d2bb7207fa634e0700
Najam <85332859+najmiter@users.noreply.github.com>
2023-12-11 12:54:16 +0500

    Update main.js
    Dynamic input handling

--- main.js
+++ main.js
@@ -1,65 +1,3 @@
-const code = `
-include irvine32.inc
-
-.data
-  first   dd  1011
-  second  dd  80
-  third   dd  25
-
-  ye_akhri_hai        db  0
-  the_gorgeous_number dd  0
-
-  pretty_msg          byte    "The pretty number is >>> ", 0
-
-.code
-main proc
-  mov eax, first
-  mov ebx, second
-
-  cmp eax, ebx
-  jg move_first_operand
-  jmp move_second_operand
-
-check_:
-  cmp ye_akhri_hai, 1
-  je ouwt
-  
-  mov ye_akhri_hai, 1
-
-  mov eax, the_gorgeous_number
-  mov ebx, third
-  cmp eax, ebx
-  jg  move_first_operand
-  jmp move_second_operand
-
-move_first_operand:
-  mov the_gorgeous_number, eax
-  jmp check_
-  
-move_second_operand:
-  mov the_gorgeous_number, ebx
-  jmp check_
-
-
-ouwt:
-  mov edx, offset pretty_msg
-  call WriteString
-
-  mov eax, the_gorgeous_number
-  call WriteDec
-
-  call CRLF   
-  call ReadChar
-  
-  invoke ExitProcess, 0
-main endp
-end main
-`.split('\n');
-
-
-const print = console.log;
-const DOM = []
-
 const Chitter = {
   asm: {
     arithmetics: new Set([
@@ -89,83 +27,76 @@ const Chitter = {
   }
 }
 
+const hightlight_btn = document.getElementById("btn-highlight");
+hightlight_btn.addEventListener("click", () => {
+    // const print = console.log;
+    const DOM = []
 
-for (const line of code) {
-  const tokens = line.split(' ');
-  const line_ = [];
+    const code = document.getElementById("input-text").value.split("\n");
+    const print = console.log;
+    // print(code);
 
-  print(tokens);
-  let spaces = 0;
-  while (tokens[++spaces] === '');
+    const DOM = [];
 
-  for (let i = 0; i < tokens.length; i++) {
-    let token = tokens[i];
-    let klass = 'plain';
-    let is_jump = false;
+    for (const line of code) {
+        const tokens = line.split(" ");
+        const line_ = [];
+        // print(tokens);
 
-    let space_not_added = true;
-    // let count = 0;
+        let spaces = 0;
+        while (tokens[spaces++] === "");
 
-    if (token.length === 0 && space_not_added) {
-      line_.push('');
-      space_not_added = false;
-      continue;
-    }
+        for (let i = 0; i < tokens.length; i++) {
+            let token = tokens[i];
+            let klass = "plain";
+            let is_jump = false;
 
-    // in_middle = true;
+            if (!isNaN(token)) {
+                klass = "constant";
+            } else if (token.endsWith(":")) {
+                klass = "function-label";
+            } else {
+                for (const key of Object.keys(Chitter.asm)) {
+                    if (Chitter.asm[key].has(token.toUpperCase())) {
+                        klass = key;
 
-    let comma = '';
-    if (token.includes(',')) {
-      token = token.substring(0, token.indexOf(','));
-      comma = ',';
-    }
+                        if (key === "jumps") {
+                            is_jump = true;
 
-    if (!isNaN(token)) {
-      klass = 'constant'
-    } else if (token.endsWith(':')) {
-      klass = 'function-label';
-    } else {
-      for (const key of Object.keys(Chitter.asm)) {
-        if (Chitter.asm[key].has(token.toUpperCase())) {
-          klass = key;
+                            const t = tokens[i];
+                            // print(t)
+                            line_.push(
+                                `<span class="function-label">${t}</span>`
+                            );
+                        }
+                        break;
+                    }
+                }
+            }
 
-          if (key === 'jumps') {
-            is_jump = true;
-            print(key)
+            let empty = "";
+            while (--spaces >= 0) empty += "&nbsp";
+            // print(empty);
 
-            // print(tokens)
-            while (tokens[++i] === '');
+            if (is_jump) {
+                line_.unshift(`<span class="${klass}">${token}</span>`);
+            } else {
+                line_.push(`<span class="${klass}">${token}</span>`);
+            }
 
-            const t = tokens[i];
-            // print(t)
-            line_.push(`<span class="function-label">${t}</span>`);
-          }
-          break;
+            // if (comma.length !== 0) {
+            //     line_.push(`<span class="plain">,</span>`);
+            // }
         }
-      }
+
+        const newbie = document.createElement("code");
+        newbie.innerHTML = line_.join(" ") + "<br>";
+
+        DOM.push(newbie);
     }
+    const output = document.getElementById("output-text");
+    output.innerHTML = "";
+    // print(DOM);
+    DOM.forEach((e) => output.appendChild(e));
+});
 
-    let empty = '';
-    while (--spaces >= 0) empty += ' ';
-
-    if (is_jump) {
-      line_.unshift(`${empty}<span class="${klass}">${token}</span>`);
-    } else {
-      line_.push(`<span class="${klass}">${token}</span>`);
-    }
-
-    if (comma.length !== 0) {
-      line_.push(`<span class="plain">,</span>`);
-    }
-  }
-
-  const newbie = document.createElement('code');
-  newbie.innerHTML = line_.join(' ') + '<br>';
-
-  DOM.push(newbie);
-}
-
-const pre = document.createElement('pre')
-const body = document.querySelector('body');
-body.appendChild(pre);
-DOM.forEach(e => pre.appendChild(e));
