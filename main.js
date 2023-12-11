915f95c99b04a773261eb18c4674c0abba785ab8
Najam <85332859+najmiter@users.noreply.github.com>
2023-12-11 17:29:38 +0500

    Update main.js
    Highlight the destination registers and put the comma without an empty space

--- main.js
+++ main.js
@@ -1,69 +1,71 @@
 const Chitter = {
-  asm: {
-    arithmetics: new Set([
-      "ADD", "SUB", "INC", "DEC", "MUL", "IMUL", "DIV", "IDIV", "AND", "OR", "XOR", "NOT", "SHL", "SHR",
-    ]),
-    criticals: new Set([
-      "RET", "PROC", "ENDP", "END"
-    ]),
-    instructions: new Set([
-      "MOV", "MOVS", "MOVSX", "MOVZX", "CMP",
-      "PUSH", "POP",
-      "LEA", "NOP", "HLT", "INT", "LEAVE", "CLC", "STC", "CLD", "STD", "CLI", "STI",
-      "CMPXCHG", "XCHG", "BSWAP", "NOP", "PUSHF", "POPF", "REP", "REPE", "REPZ",
-      "REPNE", "REPNZ", "CMC", "CWDE", "CDQ", "WAIT", "CBW", "CWD", "INTO", "IRET", "INCLUDE",
-    ]),
-    datatypes: new Set([
-      "BYTE", "WORD", "DWORD", "QWORD", "DB", "DW", "DD", "DQ", "REAL"
-    ]),
-    jumps: new Set([
-      "JMP", "JE", "JNE", "JG", "JGE", "JL", "JLE", "JZ", "JNZ", "JS", "JNS", "JC", "JNC", "JB", "JA", "CALL", "INVOKE",
-    ]),
-    registers: new Set([
-      "AL", "BL", "CL", "DL", "AH", "BH", "CH", "DH", 'AX', 'BX', 'CX',
-      'DX', 'EAX', 'EBX', 'ECX', 'EDX', 'RAX', 'RBX', 'RCX', 'RDX',
-      'DI', 'SI', 'EDI', 'ESI', 'RDI', 'RSI',
-    ]),
-  }
+      asm: {
+            arithmetics: new Set([
+                "ADD", "SUB", "INC", "DEC", "MUL", "IMUL", "DIV", "IDIV", "AND", "OR", "XOR", "NOT", "SHL", "SHR",
+            ]),
+            criticals: new Set([
+                  "RET", "PROC", "ENDP", "END"
+            ]),
+            instructions: new Set([
+                  "MOV", "MOVS", "MOVSX", "MOVZX", "CMP",
+                  "PUSH", "POP",
+                  "LEA", "NOP", "HLT", "INT", "LEAVE", "CLC", "STC", "CLD", "STD", "CLI", "STI",
+                  "CMPXCHG", "XCHG", "BSWAP", "NOP", "PUSHF", "POPF", "REP", "REPE", "REPZ",
+                  "REPNE", "REPNZ", "CMC", "CWDE", "CDQ", "WAIT", "CBW", "CWD", "INTO", "IRET", "INCLUDE",
+            ]),
+            datatypes: new Set([
+                  "BYTE", "WORD", "DWORD", "QWORD", "DB", "DW", "DD", "DQ", "REAL"
+            ]),
+            jumps: new Set([
+                  "JMP", "JE", "JNE", "JG", "JGE", "JL", "JLE", "JZ", "JNZ", "JS", "JNS", "JC", "JNC", "JB", "JA", "CALL", "INVOKE",
+            ]),
+            registers: new Set([
+                  "AL", "BL", "CL", "DL", "AH", "BH", "CH", "DH", 'AX', 'BX', 'CX',
+                  'DX', 'EAX', 'EBX', 'ECX', 'EDX', 'RAX', 'RBX', 'RCX', 'RDX',
+                  'DI', 'SI', 'EDI', 'ESI', 'RDI', 'RSI',
+            ]),
+      }
 }
 
 const hightlight_btn = document.getElementById("btn-highlight");
 hightlight_btn.addEventListener("click", () => {
-    // const print = console.log;
-
-    const code = document.getElementById("input-text").value.split("\n");
+    code = document.getElementById("input-text").value.split("\n");
     const print = console.log;
     // print(code);
 
     const DOM = [];
 
     for (const line of code) {
-        const tokens = line.split(" ");
+        const tokens = line.match(/[^ ,]+|,/g) ?? line;
         const line_ = [];
-        // print(tokens);
-
-        let spaces = 0;
-        while (tokens[spaces++] === "");
 
         for (let i = 0; i < tokens.length; i++) {
             let token = tokens[i];
             let klass = "plain";
-            let is_jump = false;
+            let already_been_added = false;
 
             if (!isNaN(token)) {
                 klass = "constant";
             } else if (token.endsWith(":")) {
                 klass = "function-label";
+            } else if (tokens[i + 1] === ",") {
+                already_been_added = true;
+                line_.push(
+                    `<span class="registers">${token}<span class="plain">,</span></span>`
+                );
+                i++;
             } else {
                 for (const key of Object.keys(Chitter.asm)) {
                     if (Chitter.asm[key].has(token.toUpperCase())) {
                         klass = key;
 
                         if (key === "jumps") {
-                            is_jump = true;
+                            already_been_added = true;
+                            line_.unshift(
+                                `<span class="${klass}">${token}</span>`
+                            );
 
-                            const t = tokens[i];
-                            // print(t)
+                            const t = tokens[++i];
                             line_.push(
                                 `<span class="function-label">${t}</span>`
                             );
@@ -72,20 +74,8 @@ hightlight_btn.addEventListener("click", () => {
                     }
                 }
             }
-
-            let empty = "";
-            while (--spaces >= 0) empty += "&nbsp";
-            // print(empty);
-
-            if (is_jump) {
-                line_.unshift(`<span class="${klass}">${token}</span>`);
-            } else {
+            if (!already_been_added)
                 line_.push(`<span class="${klass}">${token}</span>`);
-            }
-
-            // if (comma.length !== 0) {
-            //     line_.push(`<span class="plain">,</span>`);
-            // }
         }
 
         const newbie = document.createElement("code");
@@ -98,4 +88,3 @@ hightlight_btn.addEventListener("click", () => {
     // print(DOM);
     DOM.forEach((e) => output.appendChild(e));
 });
-
