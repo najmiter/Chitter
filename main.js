5768cd93da35619062f42e27c1dd8edc7d836c16
Najam <85332859+najmiter@users.noreply.github.com>
2023-12-12 15:42:02 +0500

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,6 @@ hightlight_btn.addEventListener("click", () => {
                 klass = "function-label";
             } else if (token.endsWith(",")) {
                 already_been_added = true;
-                token.replace(",", "");
                 line_.push(
                     `<span class="registers">${token.replace(
                         ",",
