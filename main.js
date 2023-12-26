8e21f1cae72da93f91ed5bb2c87bc50355715ad0
Najam <85332859+najmiter@users.noreply.github.com>
2023-12-26 23:08:55 +0500

    Update main.js

--- main.js
+++ main.js
@@ -180,6 +180,7 @@ const chittify = () => {
     DOM.forEach((e) => pre.appendChild(e));
 
     output.appendChild(pre);
+    window.location.href += "#output"; // for mobile users (probably some other way but for now, it is what it is)
 };
 
 const hightlight_btn = document.getElementById("btn-highlight");
